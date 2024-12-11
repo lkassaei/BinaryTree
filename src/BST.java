3ecacebd541b4953e05c01004d24864da9052822
lkassaei <lily.kassaei@menloschool.org>
2024-12-11 12:55:49 -0800

    Finished the sorting and insert functions.

--- src/BST.java
+++ src/BST.java
@@ -48,31 +48,104 @@ public class BST {
      */
     public boolean search(int val) {
         // TODO: Complete the search function
-        return false;
+        // Allows us to have access to the value and the current node
+        return searchHelper(val, this.root);
+    }
+
+    public boolean searchHelper(int val, BSTNode current) {
+        // If the value is the same as the value of the current node, we found it
+        if (val == current.getVal()) {
+            return true;
+        }
+        // If the current node has no children, we know it is a leaf and that we went through
+        // Everything without finding the value, so we can return false
+        else if (current.getLeft() == null && current.getRight() == null) {
+            return false;
+        }
+        // If the value is less than the current value, go left
+        else if (val < current.getVal()) {
+            return searchHelper(val, current.getLeft());
+        }
+        // If the value is more than the current value, go right
+        else if (val > current.getVal()) {
+            return searchHelper(val, current.getRight());
+        }
+        else {
+            return false;
+        }
     }
 
     /**
-     * @return ArrayList of BSTNodes in inorder
+     * @return ArrayList of BSTNodes in inorder (left, root, right)
      */
     public ArrayList<BSTNode> getInorder() {
         // TODO: Complete inorder traversal
-        return null;
+        // Create a new array list to store the nodes, the make a helper function that allows us
+        // Access to the current node and the array
+        ArrayList<BSTNode> arr = new ArrayList<BSTNode>();
+        getInorderHelper(this.root, arr);
+        return arr;
+    }
+
+    // Because ArrayLists pass references, we don't need to return the ArrayList because the
+    // Original just gets edited so we can return the original in the previous function
+    public void getInorderHelper(BSTNode current, ArrayList<BSTNode> arr) {
+        // If there is a left child, keep going left
+        if (current.getLeft() != null) {
+            getInorderHelper(current.getLeft(), arr);
+        }
+        // Once we hit the leftmost, say where we are
+        arr.add(current);
+        // Keep going right
+        if (current.getRight() != null) {
+            getInorderHelper(current.getRight(), arr);
+        }
     }
 
     /**
-     * @return ArrayList of BSTNodes in preorder
+     * @return ArrayList of BSTNodes in preorder (root, left, right)
      */
     public ArrayList<BSTNode> getPreorder() {
         // TODO: Complete preorder traversal
-        return null;
+        ArrayList<BSTNode> arr = new ArrayList<BSTNode>();
+        getPreorderHelper(this.root, arr);
+        return arr;
+    }
+
+    public void getPreorderHelper(BSTNode current, ArrayList<BSTNode> arr) {
+        // Say where we are immediately
+        arr.add(current);
+        // Then go left until you can't anymore
+        if (current.getLeft() != null) {
+            getPreorderHelper(current.getLeft(), arr);
+        }
+        // Then go right until you can't anymore
+        if (current.getRight() != null) {
+            getPreorderHelper(current.getRight(), arr);
+        }
     }
 
     /**
-     * @return ArrayList of BSTNodes in postorder
+     * @return ArrayList of BSTNodes in postorder (left, right, root)
      */
     public ArrayList<BSTNode> getPostorder() {
         // TODO: Complete postorder traversal
-        return null;
+        ArrayList<BSTNode> arr = new ArrayList<BSTNode>();
+        getPostorderHelper(this.root, arr);
+        return arr;
+    }
+
+    public void getPostorderHelper(BSTNode current, ArrayList<BSTNode> arr) {
+        // If there is a left child, keep going left
+        if (current.getLeft() != null) {
+            getPostorderHelper(current.getLeft(), arr);
+        }
+        // If there is a right child, go right
+        if (current.getRight() != null) {
+            getPostorderHelper(current.getRight(), arr);
+        }
+        // Say where we are
+        arr.add(current);
     }
 
     /**
@@ -83,6 +156,27 @@ public class BST {
      */
     public void insert(int val) {
         // TODO: Complete insert
+        // If value is not already in the array
+        if (!search(val)) {
+            // Update the root with the new tree
+            this.root = insertHelper(this.root, val);
+        }
+    }
+
+    public BSTNode insertHelper(BSTNode current, int val) {
+        // If where we find the place where we need to be and it doesn't exist, insert val
+        if (current == null) {
+            return new BSTNode(val);
+        }
+        // If val is less than where we are, go left
+        if (val < current.getVal()) {
+            current.setLeft(insertHelper(current.getLeft(), val));
+        }
+        // If val is more than where we are, go right
+        if (val > current.getVal()) {
+            current.setRight(insertHelper(current.getRight(), val));
+        }
+        return current;
     }
 
     /**
